77daf4e3d8ba2f0eb3ffa4f9ebac08773b0bcad2
wmwwong <wmwwong@legco.gov.hk>
2019-01-25 17:48:19 +0800

    prepare oauth

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 
-const PageOne = () => {
-    return <div><Link to="/pagetwo">gotopage 2</Link>PageOne</div>
-}
-
-const PageTwo = () =>{
-    return <div><Link to="/">gotopage 1</Link>PageTwo</div>
-}
 
 const App = () => {
     return (
-        <div>
-            <BrowserRouter>
+        <div className="ui container">            
+            <BrowserRouter>                
                 <div>
-                <Route path="/" exact component={PageOne} />                
-                <Route path="/pagetwo" exact component={PageTwo} />
+                    <Header />
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/new" exact component={StreamCreate} />
+                    <Route path="/streams/edit" exact component={StreamEdit} />
+                    <Route path="/streams/delete" exact component={StreamDelete} />
+                    <Route path="/streams/show" exact component={StreamShow} />                    
                 </div>
             </BrowserRouter>
         </div>
